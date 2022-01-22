0a0f2be2c4ac013f29d3c47649fb115e70bfafad
Harry Chen <harry.chen@uwaterloo.ca>
2022-01-21 20:44:35 -0500

    modified order in app

--- app.js
+++ app.js
@@ -6,28 +6,24 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-// const {MongoStore} = require('connect-mongo');
-const MongoStore = require("connect-mongo");
-
-
-
-const ExpressError = require('./utils/ExpressError');
-
-
 const User = require('./models/user');
-
+const helmet = require('helmet');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// const {MongoStore} = require('connect-mongo');
+const MongoStore = require("connect-mongo");
+
 // const dbUrl = process.env.DB_URL
-const dbUrl = process.env.DB_url || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 // mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -46,10 +42,42 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
-// app.use(helmet());
+
+
+const secret = process.env.SECRET || 'thesecretmessage'
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24*60*60
+})
+
+store.on("error",function(e){
+    console.log("Session store error", e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
 
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
@@ -96,38 +124,10 @@ app.use(
     })
 );
 
-const secret = process.env.SECRET || 'thesecretmessage'
-
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    secret,
-    touchAfter: 24*60*60
-})
-
-store.on("error",function(e){
-    console.log("Session store error", e)
-})
-
-const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
-    }
-}
-
-app.use(session(sessionConfig));
-app.use(flash());
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -139,10 +139,11 @@ app.use((req,res,next) => {
     next();
 })
 
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
-app.use('/', userRoutes);
-app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
